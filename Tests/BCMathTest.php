a41fdcf7e8f9840c19676fbb6b6e2c049281929f
Edward Brosens <edwardbrosens@gmail.com>
2018-01-23 20:50:26 +0100

    use correct test method naming

--- Tests/BCMathTest.php
+++ Tests/BCMathTest.php
@@ -1,5 +1,5 @@
 <?php
-
+use BCMath\BCMath;
 /**
  * Class BCMathTest
  * Test case for all functions in the BCMath wrapper class
@@ -9,20 +9,21 @@ class BCMathTest extends PHPUnit\Framework\TestCase {
     /**
      * Should support bcadd()
      */
-    public function shouldSupportBcaddTest () {}
+    public function testShouldSupportBcadd () {
+    }
 
     /**
      * Should support bcsub()
      */
-    public function shouldSupportBcsubTest() {}
+    public function testShouldSupportBcsub() {}
 
     /**
      * Should support bcmul()
      */
-    public function shouldSupportBcmulTest() {}
+    public function testShouldSupportBcmul() {}
 
     /**
      * Should support bcdiv()
      */
-    public function shouldSupportBcdivTest() {}
+    public function testShouldSupportBcdiv() {}
 }
