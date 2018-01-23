12304063dd657a6eb0729b07411b3476a3d18241
Edward Brosens <edwardbrosens@gmail.com>
2018-01-23 20:45:30 +0100

    stub test functions

--- Tests/BCMathTest.php
+++ Tests/BCMathTest.php
@@ -5,7 +5,24 @@
  * Test case for all functions in the BCMath wrapper class
  */
 class BCMathTest extends PHPUnit\Framework\TestCase {
-    public function shouldDoTesting() {
-        $this->assertTrue(true);
-    }
+
+    /**
+     * Should support bcadd()
+     */
+    public function shouldSupportBcaddTest () {}
+
+    /**
+     * Should support bcsub()
+     */
+    public function shouldSupportBcsubTest() {}
+
+    /**
+     * Should support bcmul()
+     */
+    public function shouldSupportBcmulTest() {}
+
+    /**
+     * Should support bcdiv()
+     */
+    public function shouldSupportBcdivTest() {}
 }
